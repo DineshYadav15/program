6bc1a94ab42c94e756f83fe2f3013c818ff29e62
Dinesh Yadav <dineshyadav1518@gmail.com>
2023-09-06 18:43:55 +0530

    dbsgf

---
+++ Range.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Range
+{
+	public static void main(String args[])
+	{
+		rane();
+		
+	}
+	static void range ()
+	{
+		Scanner java=new Scanner(System.in);
+		System.out.println("enter any number");
+		int num=java.nextInt();
+		for(int i=0;i<1;i++)
+		{
+			if(num>=0&&num<=50)
+			{
+				System.out.println("it is range of number");
+			}
+			else
+			{
+				System.out.println("it is not range of number");
+			}
+		}
+	}
+}
+		
